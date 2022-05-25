b1c3d1e664361df57a67f782932eb7f09d34a4fb
Don Peat <don@caleos.io>
2022-05-25 04:10:55 -0500

    refactor: extend error handling to include package method

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ import { S3Client, PutObjectCommand} from '@aws-sdk/client-s3';
 import { getProducers } from "@telosnetwork/validator-checks";
 
 const s3Client = new S3Client({ region: 'us-east-1'});
+
 const run = async (bucketParams) => {
-    try {
-      await s3Client.send(new PutObjectCommand(bucketParams))
-      console.log(
-        `Successfully uploaded object:${bucketParams.Bucket}/${bucketParams.Key}`
-      )
-    } catch (err) {
-      console.log('Error', err)
-    }
+  await s3Client.send(new PutObjectCommand(bucketParams))
+  console.log(
+    `Successfully uploaded object:${bucketParams.Bucket}/${bucketParams.Key}`
+  )
 }
 
 (async ()=> {
-    const producerData = await getProducers();
+  try{
+    const producerData = await getProducers(1);
     const producerDataJson = JSON.stringify(producerData);
 
     const bucketParams = {
-        Bucket: 'telos-producer-validation',
-        ContentType: 'application/json', 
-        Key: `test-data-${Date.now()}`,   
-        Body: producerDataJson
-      };
+      Bucket: 'telos-producer-validation',
+      ContentType: 'application/json', 
+      Key: `test-data-${Date.now()}`,   
+      Body: producerDataJson
+    };
 
     await run(bucketParams);
+  } catch(err) {
+    console.error(err);
+  }
 })()
